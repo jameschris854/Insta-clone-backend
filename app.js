75195dd33c160be348357bacdee3a4661d65fadb
James christopher <68394236+jameschris854@users.noreply.github.com>
2021-06-12 19:00:51 +0530

    added procfile

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(express.static(`${__dirname}/public`));
     
     app.use(cors());
     app.options('*', cors());
+
 // app.get("/", (req, res) => {
 //   res.setHeader("Access-Control-Allow-Origin", "*");
 
